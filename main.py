9effc082801a65e0eb22b70e19b988d7b458d461
Simion Putina <simionp96@gmail.com>
2016-10-31 08:33:06 +0000

    name filtering and backup blin

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ from radio import Radio
 from time import sleep
 from spotify import Spotify
 
+_radio = Radio()
 _spotify = Spotify()
-id = _spotify.getSongID("schoolboy q that part")
-songs = _spotify.getPlaylistSongs()
-_spotify.addSongs(songs, id)
+#songName = _radio.getCurrentSong()
+#id = _spotify.getSongID(songName)
+#sourceSongs = _spotify.getPlaylistSongs()
+#_spotify.addSongs(sourceSongs, id)
 
+while True:
+	try:
+		songName = _radio.getCurrentSong()
+		id = _spotify.getSongID(songName)
+		sourceSongs = _spotify.getPlaylistSongs()
+		_spotify.addSongs(sourceSongs, id)
+		_radio.closeBrowser()
+		sleep(120)
+	except Exception as e:
+		print("failed to add song {} . {}".format(songName, str(e))) 		
+		pass
+
+#print (_radio.checkSongName("Alabama Shakes - Future People (Official Video - Live from Capitol Studio A) --- Alabama Shakes - Future People (Official Video - Live from Capitol Studio A)"))
 #_radio = Radio()
 #song = _radio.getCurrentSong()
 #sleep(5)
--- radio.py
+++ radio.py
@@ -2,22 +2,50 @@ from time import sleep
 from contextlib import closing
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import re
 
 class Radio:
     def __init__(self):
         self.url = "https://treesradio.com"
-        self.driver = webdriver.Chrome()
+        
     # use firefox to get page with javascript generated content
     
     def getCurrentSong(self):
+        self.driver = webdriver.Chrome()
         self.driver.get(self.url)
         sleep(10) 
-        name = self.driver.find_element_by_class_name('current-playing-media').text
-        print(name)
-        return name
+        song = self.driver.find_element_by_class_name('current-playing-media').text
+        #song = "Joey Fatts - Choppa [ft. ASAP Rocky & Danny Brown] --- Joey Fatts - Choppa [ft. ASAP Rocky & Danny Brown]"
+        
+        #remove duplicated name
+        if ("---" in song):
+            song = song[0:song.index("---")-1]
+        
+        #disallowed characters
+        song2 = re.sub('.', lambda m: {'-':'', ':':'', '\'':''}.get(m.group(), m.group()), song)
+        
+        print("song before regex " +song)
+        #check for brackets and contents inside it
+        garbage = re.search(r"(\([A-Za-z0-9_ &^%$£!.;:<>+~#,()]+)\)", song2)
+        if (garbage == None):
+            garbage = re.search(r"\[([A-Za-z0-9_ &^%$£!.;:<>+~#,()]+)\]", song2)
+        
+        #if found, remove it
+        if (garbage != None):
+            if (garbage.group() in song2):
+                song2 = song2.replace(garbage.group(), "")
+       
+        print("song after regex" + song2)
+        return song2
     
     def closeBrowser(self):
         self.driver.close()
+    
+    def checkSongName(self, song):
+        song2 = ""
+        if ("---" in song):
+            song2 = song[0:song.index("---")-1]
+        return song2
         
     #url = "https://treesradio.com"
     #driver = webdriver.Chrome()
